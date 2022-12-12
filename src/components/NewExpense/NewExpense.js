6f3a5cfb63db851e90c11cd7bf67015bfdad0808
anaysoul <soulyana_lakew@outlook.com>
2022-12-12 16:11:27 -0500

    child-to-parent component communication (bottom-up)

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ExpenseForm from './ExpenseForm.js';
+import ExpenseForm from "./ExpenseForm.js";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
   return (
     <div className="new-expense">
-     <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
     </div>
   );
 };
